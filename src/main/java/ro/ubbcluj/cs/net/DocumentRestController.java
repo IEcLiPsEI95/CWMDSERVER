efcc7a61d55daafc3b69e90cecffe6f94b3deb13
Horatiu Stefan LUPEAN <hlupean@bitdefender.com>
2017-01-21 16:41:20 +0200

    Fixed the last fix

--- src/main/java/ro/ubbcluj/cs/net/DocumentRestController.java
+++ src/main/java/ro/ubbcluj/cs/net/DocumentRestController.java
@@ -3,7 +3,13 @@ package ro.ubbcluj.cs.net;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ro.ubbcluj.cs.controller.UserController;
+import ro.ubbcluj.cs.domain.CWMDRequestResponse;
+import ro.ubbcluj.cs.domain.User;
+import ro.ubbcluj.cs.domain.UserPerm;
 import ro.ubbcluj.cs.session.SessionManager;
 import ro.ubbcluj.cs.controller.DocumentController;
 
@@ -32,6 +38,39 @@ public class DocumentRestController
         log.info("s-o conectat: " + id);
         return Integer.toString(id * 2);
     }
-
+    
+    @RequestMapping(value = "sign", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<?> Sign(@RequestHeader(value = TOKEN_HEADER, required = true) String token,
+                                                @RequestBody int documentId)
+    {
+        try{
+            User user = sm.GetLoggedInUser(token, UserPerm.PERM_SIGN_DOCUMENT);
+            ctrlDocs.Sign(documentId, user);
+            return CWMDRequestResponse.createResponse("OK", HttpStatus.OK);
+        }
+        catch (UserController.RequestException e) {
+            log.error(e.getMessage());
+            return CWMDRequestResponse.createResponse(e.getMessage(), e.getStatus());
+        }
+        
+    }
+    
+    @RequestMapping(value = "reject", method = RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity<?> Reject(@RequestHeader(value = TOKEN_HEADER, required = true) String token,
+                             @RequestBody int documentId)
+    {
+        try{
+            User user = sm.GetLoggedInUser(token, UserPerm.PERM_SIGN_DOCUMENT);
+            ctrlDocs.Reject(documentId, user);
+            return CWMDRequestResponse.createResponse("OK", HttpStatus.OK);
+        }
+        catch (UserController.RequestException e) {
+            log.error(e.getMessage());
+            return CWMDRequestResponse.createResponse(e.getMessage(), e.getStatus());
+        }
+        
+    }
+    
 }
 
